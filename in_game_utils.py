893a0bb307b330d8c1de1ca6abfaff0ce904fe33
Almas1202 <90976381+Almas1202@users.noreply.github.com>
2022-03-08 21:23:46 +0500

    Create in_game_utils.py

---
+++ in_game_utils.py
@@ -0,0 +1,46 @@
+#Настройки окна
+W = 700
+H = 400
+FPS = 60
+
+#Цвета
+black = (1, 1, 1)
+white = (254, 254, 254)
+yellow = (245, 184, 0)
+grey = (50, 50, 50)
+
+add_speed = True
+start = False
+get_ticks = True
+title_up = True
+bot_hit = True
+can_hit = True
+pl2_hit = True
+win = False
+lose = False
+#Переменные
+pw = 15
+ph = 80
+x = 20
+y = H/2 - 50
+x2 = 665
+pos = 0, 0
+title_pos_y = 0
+posx, posy = 345, 190
+
+line_w = 10
+score1 = 0
+score2 = 0
+time = 3
+repeats = 0
+time_gone = 0
+
+moving = 'up'
+player_moving = 'stop'
+player2_moving = 'stop'
+
+speedx = 0
+speedy = 0
+
+gamemode = 0
+
